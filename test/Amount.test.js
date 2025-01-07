dc7e549d843f768c174244d0dd0d528534fca712
Evgeny Vlasenko <mahnunchik@gmail.com>
2025-01-07 15:51:49 -0600

    feat: handle negative amount

---
+++ test/Amount.test.js
@@ -0,0 +1,96 @@
+import assert from 'assert/strict';
+
+import {
+  Amount,
+} from '@coinspace/cs-common';
+
+
+describe('Amount', () => {
+  describe('#constructor', () => {
+    it('should create valid small amount with decimals', () => {
+      const amount = new Amount(1, 6);
+      assert.equal(amount.value, 1n);
+      assert.equal(amount.decimals, 6);
+      assert.equal(amount.toString(), '0.000001');
+    });
+
+    it('should create valid big amount with decimals', () => {
+      const amount = new Amount(1234567890, 6);
+      assert.equal(amount.value, 1234567890n);
+      assert.equal(amount.decimals, 6);
+      assert.equal(amount.toString(), '1234.56789');
+    });
+
+    it('should create valid amount without decimals', () => {
+      const amount = new Amount(1234567890, 0);
+      assert.equal(amount.value, 1234567890n);
+      assert.equal(amount.decimals, 0);
+      assert.equal(amount.toString(), '1234567890');
+    });
+
+    it('should create valid small negative amount with decimals', () => {
+      const amount = new Amount(-1, 6);
+      assert.equal(amount.value, -1n);
+      assert.equal(amount.decimals, 6);
+      assert.equal(amount.toString(), '-0.000001');
+    });
+
+    it('should create valid big negative amount with decimals', () => {
+      const amount = new Amount(-1234567890, 6);
+      assert.equal(amount.value, -1234567890n);
+      assert.equal(amount.decimals, 6);
+      assert.equal(amount.toString(), '-1234.56789');
+    });
+
+    it('should create valid negative amount without decimals', () => {
+      const amount = new Amount(-1234567890, 0);
+      assert.equal(amount.value, -1234567890n);
+      assert.equal(amount.decimals, 0);
+      assert.equal(amount.toString(), '-1234567890');
+    });
+  });
+
+  describe('#fromString', () => {
+    it('should create valid small amount with decimals', () => {
+      const amount = Amount.fromString('0.000001', 6);
+      assert.equal(amount.value, 1n);
+      assert.equal(amount.decimals, 6);
+      assert.equal(amount.toString(), '0.000001');
+    });
+
+    it('should create valid big amount with decimals', () => {
+      const amount = Amount.fromString('1234.567890', 6);
+      assert.equal(amount.value, 1234567890n);
+      assert.equal(amount.decimals, 6);
+      assert.equal(amount.toString(), '1234.56789');
+    });
+
+    it('should create valid amount without decimals', () => {
+      const amount = Amount.fromString('1234567890', 0);
+      assert.equal(amount.value, 1234567890n);
+      assert.equal(amount.decimals, 0);
+      assert.equal(amount.toString(), '1234567890');
+    });
+
+    it('should create valid small negative amount with decimals', () => {
+      const amount = Amount.fromString('-0.000001', 6);
+      assert.equal(amount.value, -1n);
+      assert.equal(amount.decimals, 6);
+      assert.equal(amount.toString(), '-0.000001');
+    });
+
+    it('should create valid big negative amount with decimals', () => {
+      const amount = Amount.fromString('-1234.567890', 6);
+      assert.equal(amount.value, -1234567890n);
+      assert.equal(amount.decimals, 6);
+      assert.equal(amount.toString(), '-1234.56789');
+    });
+
+    it('should create valid negative amount without decimals', () => {
+      const amount = Amount.fromString('-1234567890', 0);
+      assert.equal(amount.value, -1234567890n);
+      assert.equal(amount.decimals, 0);
+      assert.equal(amount.toString(), '-1234567890');
+    });
+  });
+});
